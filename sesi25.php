4016617cfdd429066d16a75cc3053932a3782452
ArmanSupriadi <armansupriadi4@gmail.com>
2023-10-17 20:58:20 +0700

    tugas sesi 25

---
+++ sesi25.php
@@ -0,0 +1,37 @@
+<?php
+    //Nomor 1
+    $nomor =10;
+
+    if ($nomor % 2 == 0){
+        echo "Bilangan Genap <br>";
+    } else {
+        echo "Bilangan Ganjil <br>";
+    }
+
+
+    //Nomor 2
+    $tahun = 2000;
+
+    if ($tahun % 4 == 0) {
+        echo " Tahun Kabisat <br>";
+    } else {
+    echo " Bukan Tahun Kabisat <br> ";
+    }
+
+
+    //Nomor 3
+    $nilai =59;
+
+    if ($nilai >= 90) {
+        echo "Grade A";
+    } else if ($nilai >= 80) {
+        echo "Grade B";
+    }else if ($nilai >= 70) {
+        echo "Grade C";
+    } else if ($nilai >= 60) {
+        echo "Grade D";
+    } else {
+        echo "Grade E";
+    }
+ ?>
+
