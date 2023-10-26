a855b05a0b9ce2f0ab0ecbd8a208c24be49dbe6a
ArmanSupriadi <armansupriadi4@gmail.com>
2023-10-27 05:52:26 +0700

    Bangun datar

---
+++ bangun_datar.php
@@ -0,0 +1,49 @@
+<?php 
+
+//Luas Persegi Panjang
+$p = 12; 
+$l = 24;
+
+$luaspp = $p * $l ;
+
+echo "Luas Persegi Panjang : ".$luaspp;
+
+echo "<br>";
+
+//Luas Persegi
+$s = 10;
+
+$luasp = $s * $s;
+
+echo "Luas Persegi : ".$luasp;
+
+echo "<br>";
+
+//Luas Segitiga
+$as = 7;
+$ts = 14;
+
+$luass = 0.5 * $as * $ts ;
+
+echo "Luas Segitiga : ".$luass;
+
+echo "<br>";
+
+//Luas Jajar Genjang
+$ajg = 15; //alas jajar genjang.
+$tjg = 7;
+
+$luasjg = $ajg * $tjg;
+
+echo "Luas Jajar Genjang : ".$luasjg;
+
+echo "<br>";
+
+//Luas Belah Ketupat
+$d1 = 12;
+$d2 = 6;
+
+$luasbk = 0.5 * $d1 * $d2;
+
+echo "Luas Belah Ketupat : ".$luasbk;
+?>
