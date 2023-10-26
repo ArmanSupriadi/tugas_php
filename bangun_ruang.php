839b0b857190ef8635c7ee8308ddc9668fb772a4
ArmanSupriadi <armansupriadi4@gmail.com>
2023-10-27 05:50:56 +0700

    Tugas Sesi 27

---
+++ bangun_ruang.php
@@ -0,0 +1,29 @@
+<?php 
+
+//Volume Kubus
+$sisi = 10;
+
+$volume = $sisi * $sisi * $sisi;
+
+echo "Volume Kubus : ".$volume;
+echo "<br>";
+
+//Volume Prisma
+$la = 20;
+$t = 10;
+
+$volumep = $la * $t;
+
+echo "Volume Prisma : ".$volumep;
+echo "<br>";
+
+//Volume Balok
+$pb = 20;
+$lb = 10;
+$tb = 5;
+
+$volumeb = $pb * $lb * $tb;
+
+echo "Volume Balok : ".$volumeb;
+echo "<br>";
+?>
